ef603f16a4977a3c2ebdbe5b0b0f510726190ceb
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-04-08 14:03:50 +1000

    lower case button added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,6 +6,10 @@ export const TextForm = (props) => {
     let newText = text.toUpperCase();
     setText(newText);
   };
+  const lowerClick = () => {
+    let lowerText = text.toLowerCase();
+    setText(lowerText);
+  };
 
   const clear = () => {
     setText("");
@@ -30,8 +34,11 @@ export const TextForm = (props) => {
       <div className="btn btn-dark" onClick={upperClick}>
         Convert to UPPER CASE
       </div>{" "}
-      <div className="btn btn-dark" onClick={clear}>
+      <div className="btn btn-danger" onClick={clear}>
         Clear
+      </div>{" "}
+      <div className="btn btn-primary" onClick={lowerClick}>
+        convert to lower case
       </div>
     </Form>
   );
