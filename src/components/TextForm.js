b220f0a59b7853e1d11db9b8be2bee2dbdce064e
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-04-08 22:34:41 +1000

    copying and removing extra space function

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,17 +2,28 @@ import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 
 export const TextForm = (props) => {
-  const upperClick = () => {
+  const handleUpperClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
   };
-  const lowerClick = () => {
+  const handleLowerClick = () => {
     let lowerText = text.toLowerCase();
     setText(lowerText);
   };
 
-  const clear = () => {
+  const handleClear = () => {
     setText("");
+  };
+
+  const handleCopy = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
   };
 
   const handleOnChange = (e) => {
@@ -27,19 +38,27 @@ export const TextForm = (props) => {
         <Form.Control
           as="textarea"
           value={text}
+          id="myBox"
           onChange={handleOnChange}
           rows={8}
         />
       </Form.Group>
-      <div className="btn btn-dark mx-2" onClick={upperClick}>
+      <div className="btn btn-dark mx-2" onClick={handleUpperClick}>
         Convert to UPPER CASE
       </div>
-      <div className="btn btn-danger mx-2" onClick={clear}>
+      <div className="btn btn-danger mx-2" onClick={handleClear}>
         Clear
       </div>
-      <div className="btn btn-primary mx-2" onClick={lowerClick}>
+      <div className="btn btn-primary mx-2" onClick={handleLowerClick}>
         convert to lower case
       </div>
+      <div className="btn btn-primary mx-2" onClick={handleCopy}>
+        Copy
+      </div>
+      <div className="btn btn-primary mx-2" onClick={handleExtraSpaces}>
+        Remove Extra Space
+      </div>
+
       <div className="container my-5">
         <h2>Your Text Summary</h2>
         <p>
